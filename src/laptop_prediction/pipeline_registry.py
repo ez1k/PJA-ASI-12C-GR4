cfea6d8e13bbcf08496a75f2bc660a1b56f40a47
mabi0110 <biziewskamk@gmail.com>
2024-05-31 23:17:13 +0200

    add old project structure

--- src/laptop_prediction/pipeline_registry.py
+++ src/laptop_prediction/pipeline_registry.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-from laptop_prediction.pipelines.automl import pipeline as automl_pipeline
-from laptop_prediction.pipelines.ml import pipeline as ml_pipeline
+
 
 def register_pipelines() -> dict[str, Pipeline]:
-    return {
-        "automl": automl_pipeline.create_pipeline(),
-        "ml": ml_pipeline.create_pipeline(),
-        "__default__": ml_pipeline.create_pipeline()
-    }
+    """Register the project's pipelines.
+
+    Returns:
+        A mapping from pipeline names to ``Pipeline`` objects.
+    """
+    pipelines = find_pipelines()
+    pipelines["__default__"] = sum(pipelines.values())
+    return pipelines
--- src/laptop_prediction/pipelines/automl/pipeline.py
+++ src/laptop_prediction/pipelines/automl/pipeline.py
@@ -5,30 +5,11 @@ generated using Kedro 0.19.2
 
 from kedro.pipeline import Pipeline, pipeline, node
 
-from laptop_prediction.pipelines.automl.nodes import evaluate_models, preprocess_data, split_data, train_model_challenger, train_model_champion, transform_data
-
+from laptop_prediction.pipelines.automl.nodes import evaluate_models, train_model_challenger, train_model_champion
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-        node(
-                func=transform_data,
-                inputs="laptops",
-                outputs="laptops_for_model",
-                name="transform_laptops_node"
-            ),
-        node(
-                func=preprocess_data,
-                inputs="laptops_for_model",
-                outputs="data",
-                name="preprocess_data_node",
-            ),
-        node(
-                func=split_data,
-                inputs=["data", "params:test_size"],
-                outputs=["train_data", "test_data"],
-                name="split_data_node",
-            ),
         node(
                 func=train_model_champion,
                 inputs="train_data",
@@ -44,7 +25,7 @@ def create_pipeline(**kwargs) -> Pipeline:
         node(
                 func=evaluate_models,
                 inputs=["model_champion", "model_challenger", "test_data"],
-                outputs="best_model",
+                outputs="best_model_ml",
                 name="evaluate_models_node",
             )
     ])
