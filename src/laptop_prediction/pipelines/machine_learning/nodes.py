77f0bacf23650dd1676fea29eaa12bca549537d5
Norbert Isański <s20527@pjwstk.edu.pl>
2024-06-26 15:14:25 +0200

    retraining

--- src/laptop_prediction/pipelines/machine_learning/nodes.py
+++ src/laptop_prediction/pipelines/machine_learning/nodes.py
@@ -1,3 +1,7 @@
+"""
+This is a boilerplate pipeline 'ml'
+generated using Kedro 0.19.2
+"""
 import os
 import pandas as pd
 import numpy as np
@@ -6,9 +10,8 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import math
-import wandb
-import joblib
-
+import pickle
+import json
 
 def run_model(X_train: pd.DataFrame, y_train: pd.DataFrame, forest_n: int) -> RandomForestRegressor:
     model = RandomForestRegressor(n_estimators=forest_n, random_state=0)
@@ -26,53 +29,72 @@ def optimize_model(model: RandomForestRegressor, X_test: pd.DataFrame, y_test: p
     
     random_search = RandomizedSearchCV(estimator=model, param_distributions=params, n_iter=100, cv=cv, verbose=verbose, random_state=0, n_jobs=n_jobs)
     random_search.fit(X_test, y_test.values.ravel())
-    best_model = random_search.best_estimator_
-    return best_model
+    model_challenger = random_search.best_estimator_
+    return model_challenger
 
-def compare_with_old_model(new_model: RandomForestRegressor, X_val: pd.DataFrame, y_val: pd.DataFrame, retraining: bool):
-    model_path = 'data/04_model/best_model.pickle'
+def evaluate_model(model_challenger: RandomForestRegressor, X_val: pd.DataFrame, y_val: pd.DataFrame, retraining: bool, test_data: pd.DataFrame) -> dict:
+    if (retraining):
+        best_model_path = 'data/04_model/best_model.pickle'
+        best_score_path = "data/05_model_output/score_best_model.json"
+        if os.path.exists(best_model_path) and os.path.exists(best_score_path):
+            with open(best_model_path, 'rb') as file:
+                best_model = pickle.load(file)
+            with open(best_score_path, "r") as f:
+                best_model_score = json.load(f)
+            champion_model = best_model_score['model']
+            print('Retraining')
+            if (champion_model == 'ml'):
+                y_pred_test = best_model.predict(X_val)
+                mae = mean_absolute_error(y_val, y_pred_test)
+                mse = mean_squared_error(y_val, y_pred_test)
+                rmse = math.sqrt(mse)
+                r2 = r2_score(y_val, y_pred_test)
 
-    if(not retraining):
-        return new_model
-    if(not os.path.exists(model_path)):
-        print("Old model not available, skipping.")
-        return new_model
-    
-    y_pred_test_new_moddel = new_model.predict(X_val)
-    score_new_model = mean_squared_error(y_val, y_pred_test_new_moddel)
+                print(champion_model)
+                print(mae)
+                print(mse)
+                print(rmse)
+                print(r2)
 
-    old_model = joblib.load(model_path)
+                retraining_results = {
+                    "model": champion_model,
+                    "val_mae": mae,
+                    "val_mse": mse,
+                    "val_rmse": rmse,
+                    "val_r2": r2
+                }
+            else:
+                evaluation_results = best_model.evaluate(test_data)
+                mae = evaluation_results.get('mean_absolute_error') * -1
+                mse = evaluation_results.get('mean_squared_error') * -1
+                rmse = evaluation_results.get('root_mean_squared_error') * -1
+                r2 = evaluation_results.get('r2')
+            
+                print(champion_model)
+                print(mae)
+                print(mse)
+                print(rmse)
+                print(r2)
+            
+                retraining_results = {
+                    "model": champion_model,
+                    "val_mae": mae,
+                    "val_mse": mse,
+                    "val_rmse": rmse,
+                    "val_r2": r2
+                }
+            with open(best_score_path, "w") as f:
+                json.dump(retraining_results, f)
 
-    y_pred_test_old_model = old_model.predict(X_val)
-    score_old_model = mean_squared_error(y_val, y_pred_test_old_model)
 
-    result_model = new_model
 
-    if score_new_model < score_old_model:
-        message = "New model is better."
-    else:
-        result_model = old_model
-        message = "Old model is better."
-    
-    print(message)
-
-    return result_model
-
-def evaluate_model(best_model: RandomForestRegressor, X_val: pd.DataFrame, y_val: pd.DataFrame) -> dict:
     metrics = {}
 
-    y_pred_test = best_model.predict(X_val)
+    y_pred_test = model_challenger.predict(X_val)
+    metrics['model'] = 'ml'
     metrics['val_mae'] = mean_absolute_error(y_val, y_pred_test)
     metrics['val_mse'] = mean_squared_error(y_val, y_pred_test)
     metrics['val_rmse'] = math.sqrt(metrics['val_mse'])
     metrics['val_r2'] = r2_score(y_val, y_pred_test)   
-
-    wandb.init(project='PJA-ASI-12C-GR4')
-    wandb.log({
-        "Val MAE": metrics['val_mae'],
-        "Val MSE": metrics['val_mse'],
-        "Val RMSE": metrics['val_rmse'],
-        "Val R2": metrics['val_r2'],
-    })
 
     return metrics
--- src/laptop_prediction/pipelines/machine_learning/pipeline.py
+++ src/laptop_prediction/pipelines/machine_learning/pipeline.py
@@ -1,5 +1,5 @@
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import run_model, optimize_model, evaluate_model, compare_with_old_model
+from .nodes import run_model, optimize_model, evaluate_model
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
@@ -16,14 +16,8 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="optimize_model_node",
             ),
         node(
-                func=compare_with_old_model,
-                inputs=["best_model", "X_val", "y_val", "params:retraining"],
-                outputs="new_or_old",
-                name="compare_with_old_model_node",
-            ),
-        node(
                 func=evaluate_model,
-                inputs=["model_challenger", "X_val", "y_val"],
+                inputs=["model_challenger", "X_val", "y_val", "params:retraining", "test_data"],
                 outputs="score_challenger",
                 name="evaluate_model_node",
             )
